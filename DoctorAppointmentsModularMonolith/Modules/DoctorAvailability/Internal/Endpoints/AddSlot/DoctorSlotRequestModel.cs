b14508587b1512af7adfb6cb426f1d8261a03008
Ibrahim Megahed <Ibrahim__Hasan@outlook.com>
2025-01-07 09:28:17 +0300

    Update folder structure

--- DoctorAppointmentsModularMonolith/Modules/DoctorAvailability/Internal/Endpoints/AddSlot/DoctorSlotRequestModel.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace DoctorAvailability1.Internal.Endpoints.AddSlot;
-public record DoctorSlotRequestModel(DateTime Date, Guid DoctorId, string DoctorName, decimal Cost);
-
--- DoctorAppointmentsModularMonolith/Modules/DoctorAvailability/Internal/GlobalUsing.cs
+++
@@ -1,4 +0,0 @@
-﻿global using DoctorAvailability.Internal.Data;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.DependencyInjection;
