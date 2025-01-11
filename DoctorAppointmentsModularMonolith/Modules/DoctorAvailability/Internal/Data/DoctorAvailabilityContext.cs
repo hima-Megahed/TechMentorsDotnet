427ae37e9d374b3efb8f65311cc54b5ce45534fa
Mostafa-Selim-AboAli <mostafa.selim4@gmail.com>
2025-01-11 23:54:54 +0200

    Add files via upload

---
+++ DoctorAppointmentsModularMonolith/Modules/DoctorAvailability/Internal/Data/DoctorAvailabilityContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace DoctorAvailability.Internal.Data;
+internal class DoctorAvailabilityContext(DbContextOptions<DoctorAvailabilityContext> options)
+    : DbContext(options)
+{
+    internal DbSet<DoctorSlot> DoctorSlots { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.HasDefaultSchema("DoctorAvailability");
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+}
