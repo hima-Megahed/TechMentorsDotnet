3f0b8adaedd95b9832b947e6905fb2e2a61da1e2
Mustafa Selim <mselim@futureface.sa>
2025-01-11 19:59:03 +0200

    Remove all

--- DoctorAppointmentsModularMonolith/Modules/DoctorAvailability/Internal/Endpoints/AddSlot/DoctorSlotRequestModel.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace DoctorAvailability.Internal.Endpoints.AddSlot;
-public record DoctorSlotRequestModel(DateTime Date, Guid DoctorId, string DoctorName, decimal Cost);
-
--- DoctorAppointmentsModularMonolith/Modules/DoctorAvailability/Internal/GlobalUsing.cs
+++
@@ -1,5 +0,0 @@
-﻿global using DoctorAvailability.Internal.Data;
-global using DoctorAvailability.Internal.Models;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.DependencyInjection;
